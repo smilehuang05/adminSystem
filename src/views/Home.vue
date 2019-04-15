4c111ea55dacae16459c86e7ed6a48152733c5c2
vivi <huangyh1988@qq.com>
2019-04-15 17:05:22 +0800

    首页侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="home">
     <el-container>
+      <!-- 侧边栏 -->
       <el-aside width="200px">
-        <div class="logo"></div>
+        <el-menu default-active="2" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" class='el-menu-admin'>
+          <div class="logo"></div>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1">
+              <i class="el-icon-menu"></i>用户列表</el-menu-item>
+          </el-submenu>
+
+        </el-menu>
       </el-aside>
       <el-container>
         <el-header>Header</el-header>
@@ -10,11 +22,18 @@
       </el-container>
     </el-container>
   </div>
-
 </template>
 <script>
 import {getUserList} from '@/api'
 export default {
+  methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    },
   mounted(){
     let params = {params:{query:'',pagenum:1,pagesize:1}}
     getUserList(params).then(res=>{
@@ -26,6 +45,9 @@ export default {
 <style lang="scss" scoped>
 .home{
   height: 100%;
+  .el-menu-admin{
+    width:200px;
+  }
   .el-container{
     height: 100%;
   }
