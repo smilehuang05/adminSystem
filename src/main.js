748d7f2b31074bc84449c72e3493ac59e7f3fb7c
vivi <huangyh1988@qq.com>
2019-04-15 14:10:34 +0800

    设置路由全局守卫

--- src/main.js
+++ src/main.js
@@ -13,6 +13,23 @@ Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+//注册一个路由全局守卫，作用是在路由跳转前，对路由进行判断，防止未登录的用户跳转到其他页面去
+router.beforeEach((to, from, next) => {
+    let token = localStorage.getItem('myToken')
+    if (token) {
+        //如果已经登录，那我干涉你，随便你访问
+        next()
+    } else {
+        if (to.path !== '/login') {
+            //如果没有登录，但你访问的是其他需要登录的页面，那我就让你条到登录页面去
+            next({ path: '/login' })
+        } else {
+            //如果没有登录，访问的是login，那就不干涉，让你访问
+            next()
+        }
+    }
+})
+
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
